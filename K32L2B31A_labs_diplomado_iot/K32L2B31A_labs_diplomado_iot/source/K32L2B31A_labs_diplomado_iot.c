fd3bef2d3bf6e2a8f26bb221a96b29bfc969d8c6
victor1736 <victoralfonsofernandezhoyos@gmail.com>
2022-11-15 06:41:42 -0500

    configuración del sensor de luz en los pines y canales de adc
    se configuro el adc para el sensor de luz

--- K32L2B31A_labs_diplomado_iot/K32L2B31A_labs_diplomado_iot/source/K32L2B31A_labs_diplomado_iot.c
+++ K32L2B31A_labs_diplomado_iot/K32L2B31A_labs_diplomado_iot/source/K32L2B31A_labs_diplomado_iot.c
@@ -1,37 +1,16 @@
 /*
- * Copyright 2016-2022 NXP
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- * o Redistributions of source code must retain the above copyright notice, this list
- *   of conditions and the following disclaimer.
- *
- * o Redistributions in binary form must reproduce the above copyright notice, this
- *   list of conditions and the following disclaimer in the documentation and/or
- *   other materials provided with the distribution.
- *
- * o Neither the name of NXP Semiconductor, Inc. nor the names of its
- *   contributors may be used to endorse or promote products derived from this
- *   software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * @file   	 	:K32L2B31A_labs_diplomado_iot.c
+ * @author 		:Victor Alfonso Fernandez Hoyos
+ * @version		:0.0.000
+ * @date		:08/11/2022
+ * @brief   	:Funcion principal main
+ * @details
+ * 				v0.0.000  	Proyecto base creado usando MCUXpresso
  */
 
-/**
- * @file    K32L2B31A_labs_diplomado_iot.c
- * @brief   Application entry point.
- */
+/******************************************************************************************
+ * Include
+ *****************************************************************************************/
 #include <stdio.h>
 #include "board.h"
 #include "peripherals.h"
@@ -39,13 +18,26 @@
 #include "clock_config.h"
 #include "K32L2B31A.h"
 #include "fsl_debug_console.h"
-/* TODO: insert other include files here. */
+/******************************************************************************************
+ * Definitions
+ *****************************************************************************************/
 
-/* TODO: insert other definitions and declarations here. */
+/******************************************************************************************
+ * Private prototypes
+ *****************************************************************************************/
 
-/*
- * @brief   Application entry point.
- */
+/******************************************************************************************
+ * External vars
+ *****************************************************************************************/
+
+/******************************************************************************************
+ * Local vars
+ *****************************************************************************************/
+    volatile static int i = 0 ;
+    float voltaje=12.5f;
+/******************************************************************************************
+ * Private Source Code
+ *****************************************************************************************/
 int main(void) {
 
     /* Init board hardware. */
@@ -57,7 +49,8 @@ int main(void) {
     BOARD_InitDebugConsole();
 #endif
 
-    PRINTF("Hello World\n");
+    PRINTF("Hola mundo\r\n");
+    PRINTF("Voltaje: %2.3f\r\n",voltaje);
 
     /* Force the counter to be placed into memory. */
     volatile static int i = 0 ;
